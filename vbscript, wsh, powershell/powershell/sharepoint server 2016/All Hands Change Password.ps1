0be228de5094a8effca62982f7503ed8b187a71f
Robert Velazquez <107489923+robmvelazquez@users.noreply.github.com>
2022-10-01 07:50:00 -0700

    Add files via upload

---
+++ vbscript, wsh, powershell/powershell/sharepoint server 2016/All Hands Change Password.ps1
@@ -0,0 +1,12 @@
+﻿<# This script is used to change all passwords in the CANES Users Organizational Unit. 
+   To run this script, log into a Domain Controller and run with administrator privileges.
+   If you have any questions, ask V. #>
+
+
+#This line of code gathers all users in the CANES User Organizational Unit
+Get-ADUser -Filter * -SearchBase "OU=USA Users,OU=CANES Users,OU=CANES Users and Computers,DC=ddg60,DC=navy,DC=mil" | 
+Format-Wide DistinguishedName
+
+#This line of code forces all users to change their password
+Get-ADUser -Filter * -SearchBase “OU=USA Users,OU=CANES Users,OU=CANES Users and Computers,DC=ddg60,DC=navy,DC=mil” | 
+Set-ADUser -ChangePasswordAtLogon $true -PasswordNeverExpires $false
