8ddd254f8a31ba63ef94dd6ce99863b1c4fb915a
Robert Velazquez <107489923+robmvelazquez@users.noreply.github.com>
2022-10-01 07:54:02 -0700

    Add files via upload

---
+++ vbscript, wsh, powershell/powershell/sharepoint server 2016/Password Disable.ps1
@@ -0,0 +1,11 @@
+﻿<# This script is used to verify users that have not changed their passwords.
+   To disable accounts that have not changed their passwords, run this PowerShell
+   script on any of the Domain Controllers with administrator privileges. 
+   If you have any questions, ask V. #>
+
+
+#This line of code creates a table to view all users that have not changed their passwords.
+Get-ADUser -LDAPFilter "(pwdLastSet=0)" | Select SamAccountName,distinguishedName
+
+#This line of code disables all user accounts that have not changed their passwords.
+Get-ADUser -LDAPFilter "(pwdLastSet=0)" | Set-ADUser -Enabled $False
